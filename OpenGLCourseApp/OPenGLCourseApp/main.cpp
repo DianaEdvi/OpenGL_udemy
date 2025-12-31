f7d78f3dbe970aff6d616effa31bcffbfbdec1e5
Diana <edvi.diana@gmail.com>
2025-12-31 01:18:23 -0500

    my triangle now translates, rotates and scales. woo! except its distorted bcs i dont have a projection matrix yet

--- OpenGLCourseApp/OPenGLCourseApp/main.cpp
+++ OpenGLCourseApp/OPenGLCourseApp/main.cpp
@@ -4,27 +4,36 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
-#include <glm/>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f; // limit
 float triIncrement = 0.005f;
 
+float curAngle = 0.0f;
+float sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
+
 // Vertex shader
 static const char* vShader = "                          \n\
 #version 330                                            \n\
 														\n\
 layout (location = 0) in vec3 pos;                      \n\
-uniform float xMove;									\n\
+uniform mat4 model;										\n\
 														\n\
 void main()                                             \n\
 {                                                       \n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);       \n\
+	gl_Position = model * vec4(pos, 1.0);			    \n\
 }";
 
 static const char* fShader = "                          \n\
@@ -110,7 +119,7 @@ void CompileShaders() {
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -177,13 +186,38 @@ int main()
 			direction = !direction;
 		}
 
+		curAngle += 1.0f;
+		// prevernt overflow
+		if (curAngle >= 360) {
+			curAngle -= 360;
+		}
+
+		// scale
+		if (sizeDirection) {
+			curSize += 0.001f;
+		}
+		else {
+			curSize -= 0.001f;
+		}
+
+		// shrink/enlarge at limits
+		if (curSize >= maxSize || curSize <= minSize) {
+			sizeDirection = !sizeDirection;
+		}
+
 		// clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 
